96ea33b7eff6b0e2d4e2af1f5f4120e038edbf19
shark <szczerbicki.pawel@gmail.com>
2015-08-11 00:02:56 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,64 @@
+(function () {
+    var testData = [{
+        mark: "Opel",
+        model: "Astra",
+        photos: ["http://dreamatico.com/data_images/car/car-5.jpg", "http://www.wellclean.com/wp-content/themes/artgallery_3.0/images/car3.png"],
+        reviews: [{author: "some author", title: "some title", description: "some Desc"}, {
+            author: "sec author",
+            title: "sec title",
+            description: "sec Desc",
+            star:4
+        }],
+        description: "Description",
+        additionalData: "data"
+    },
+        {
+            mark: "VW",
+            model: "CC",
+            photos: ["http://www.bloomberg.com/ss/07/04/0418_shanghai_auto/image/audi-a6-sedan.jpg", "http://udhao.net/wp-content/uploads/Black-Audi-TTS-Car-Wallpaper.jpg"],
+            reviews: [{author: "rev author", title: "revtitle", description: "desc CC"}, {
+                author: "cc sec author",
+                title: "cc sec title",
+                description: "cc sec Desc",
+                star:5
+            }],
+            description: "CC long",
+            additionalData: "CC data"
+        }];
+    var app = angular.module("carApp", []);
+
+    app.controller("CarController", function () {
+        this.cars = testData;
+    });
+
+    app.controller("ReviewController", function () {
+        this.review = {};
+        this.addReview = function(car){
+            car.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
+    app.controller("TabsController", function () {
+        var self = this;
+        self.currentTab = 1;
+
+        this.isSet = function (tab) {
+            return self.currentTab === tab;
+        };
+
+        this.setActive = function (tab) {
+            return self.currentTab = tab;
+        };
+    });
+
+    app.controller("PhotoController", function () {
+        this.current = 0;
+        this.setCurrent = function (elem) {
+            this.current = elem ? elem : 0;
+        };
+        this.currentPhotoUrl = function(car){
+            return car.photos[current];
+        }
+    });
+})();
